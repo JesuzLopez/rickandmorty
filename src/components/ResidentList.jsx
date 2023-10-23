80e5298701960765192d705fd7f4f1a305cfbe92
Arturo <artcrash7@gmail.com>
2023-10-22 22:41:46 -0600

    pagination

--- src/components/ResidentList.jsx
+++ src/components/ResidentList.jsx
@@ -1,11 +1,39 @@
+ import { useEffect, useState } from "react"
 import ResidentCard from "./ResidentCard"
+import { paginationLogic } from "../utils/pagination"
 
-const ResidentList = ({residents}) => {
+const ResidentList = ({ residents }) => {
+    const [currentPage, setCurrentPage] = useState (1)
+
+    const { pages, residentsInPage } = paginationLogic (currentPage, residents)
+
+    useEffect(() => {
+    setCurrentPage(1)
+    }, [residents])
+
     return (
+        <section>
         <section className="grid grid-cols-[repeat(auto-fit,_280px)] justify-center gap-6 max-w-[1000px] mx-auto py-10">
-        {residents.map((resident) => (
+        {residentsInPage.map((resident) => (
              <ResidentCard key={resident} residentEndpoint={resident} />
              ))}
+        </section>
+
+        {/* Paginacion */}
+        <ul className="text-lg flex gap-3 justify-center flex-wrap">
+            {pages.map((page) => (
+                    <li key={page}> 
+                        <button
+                         className={`bg-white text-black p-2 ${
+                         page == currentPage && "bg-green-300"
+                        }`}
+                         onClick={() => setCurrentPage(page)}
+                         >
+                        {page}
+                        </button>
+                    </li>
+                ))}
+        </ul>
         </section>
     )
 }
